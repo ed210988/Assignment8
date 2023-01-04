1576366eccf23fb52096d2a48883d6a65c2b4902
Edgar Eguia <edgar.eguiacalcaneo@hotmail.com>
2023-01-03 19:48:51 -0600

    Initial state

---
+++ blogging/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from blogging.models import Post, Category
+
+# Register your models here.
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
