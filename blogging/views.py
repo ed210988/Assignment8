b286c40493a894ebfd30d11dca1b7a12c60947a1
Edgar Eguia <edgar.eguiacalcaneo@hotmail.com>
2023-01-05 20:04:18 -0600

    Adding queryset

--- blogging/views.py
+++ blogging/views.py
@@ -7,7 +7,9 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 class PostListView(ListView):
-    model = Post
+
+    queryset = Post.objects.order_by('published_date')
+    context_object_name = 'post_list'
     template_name = 'blogging/list.html'
 
 class PostDetailView(DetailView):
@@ -24,18 +26,3 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
-
-# def list_view(request):
-    # published = Post.objects.exclude(published_date__exact=None)
-    # posts = published.order_by('-published_date')
-    # context = {'posts': posts}
-    # return render(request, 'blogging/list.html', context)
-
-# def detail_view(request, post_id):
-    # published = Post.objects.exclude(published_date__exact=None)
-    # try:
-        # post = published.get(pk=post_id)
-    # except Post.DoesNotExist:
-        # raise Http404
-    # context = {'post': post}
-    # return render(request, 'blogging/detail.html', context)
